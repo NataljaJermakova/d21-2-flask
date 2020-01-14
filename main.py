c8256b75940db7cde72a776f55ac6fbfc801b07d
NataljaJermakova <natasa2912011188@gmail.com>
2020-01-14 10:08:56 +0000

    active poga

--- main.py
+++ main.py
@@ -19,4 +19,5 @@ def getAbout():
 def contact():
   return render_template('contact.html', phone = 897654321)
 
-app.run(host='0.0.0.0', port=8020)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', threaded=True, port=8020, debug=True)
