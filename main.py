a6bd7664d974a34d7571e3fd3bf661bda14acc3b
[Students] Natalja Jermakova <81DNJermakova@rvt.lv>
2020-01-31 09:52:37 +0200

    added post and text inside

--- main.py
+++ main.py
@@ -19,5 +19,10 @@ def getAbout():
 def contact():
   return render_template('contact.html', phone = 897654321)
 
+
+@app.route('post')
+def post():
+  return request.args
+
 if __name__ == '__main__':
   app.run(host='0.0.0.0', threaded=True, port=5000, debug=True)
